0102780637b136a88ef0ddd1c508f935c56e9765
vanguyen333 <vanguyen333@yahoo.com>
2023-09-11 15:13:50 -0400

    #9TypeAliases

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,15 +1,7 @@
 "use strict";
-let greet;
-// greet = "hello";//not allowed cuz greet is a function
-greet = () => {
-    console.log("hello,again");
+const logDetails = (uid, item) => {
+    console.log(`${item}has a uid of ${uid}`);
 };
-const add = (a, b, c = 10) => {
-    console.log(a + b);
-    console.log(c);
+const greet = (user) => {
+    console.log(`${user.name} says hello`);
 };
-add(5, 10);
-const minus = (a, b) => {
-    return a + b;
-};
-let result = minus(10, 7);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,18 +1,10 @@
-let greet: Function;
+type StringOrNum = string | number;
+type objWithName = { name: string; uid: StringOrNum };
 
-// greet = "hello";//not allowed cuz greet is a function
-
-greet = () => {
-  console.log("hello,again");
+const logDetails = (uid: StringOrNum, item: string) => {
+  console.log(`${item}has a uid of ${uid}`);
 };
 
-const add = (a: number, b: number, c: number | string = 10) => {
-  console.log(a + b);
-  console.log(c);
+const greet = (user: objWithName) => {
+  console.log(`${user.name} says hello`);
 };
-add(5, 10);
-
-const minus = (a: number, b: number): number => {
-  return a + b;
-};
-let result = minus(10, 7);
