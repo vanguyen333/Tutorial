a3ae0c439e9687775548b7b28d61e5a8d0933cdf
vanguyen333 <vanguyen333@yahoo.com>
2023-09-11 16:11:30 -0400

    #12 classes :

--- public/app.js
+++ public/app.js
@@ -1,10 +1,24 @@
 "use strict";
-// const anchor = document.querySelector("a")!;
-// console.log(anchor.href);
-// if (anchor) {
-//   console.log(anchor.href);
-// }
-// const form = document.querySelector("form")!;
+//classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luig website", 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invOne.client = "yoshi";
+invTwo.amount = 400;
+console.log(invOne, invTwo);
+console.log(invoices);
 const form = document.querySelector(".new-item-form");
 // console.log(form.children);
 //inputs
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,31 @@
-// const anchor = document.querySelector("a")!;
-// console.log(anchor.href);
-// if (anchor) {
-//   console.log(anchor.href);
-// }
+//classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
 
-// const form = document.querySelector("form")!;
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+  format() {
+    return `${this.client} owes $${this.amount} for ${this.details}`;
+  }
+}
+
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luig website", 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invOne.client = "yoshi";
+invTwo.amount = 400;
+console.log(invOne, invTwo);
+
+console.log(invoices);
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 // console.log(form.children);
