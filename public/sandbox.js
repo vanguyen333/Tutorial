28741be7d5d4a233781344e61ba8418d0cc464e5
vanguyen333 <vanguyen333@yahoo.com>
2023-09-11 15:26:47 -0400

    #10 function signatures

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,7 +1,22 @@
 "use strict";
-const logDetails = (uid, item) => {
-    console.log(`${item}has a uid of ${uid}`);
+// let greet: Function;
+//example
+let greet; //void cuz NOT return anything
+greet = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
 };
-const greet = (user) => {
-    console.log(`${user.name} says hello`);
+//example 2
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action === "add") {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+};
+//example 3
+let logDetails;
+logDetails = (ninja) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`);
 };
