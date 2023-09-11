f87079989a90a4d11c8f5274fa98e604032ede91
vanguyen333 <vanguyen333@yahoo.com>
2023-09-10 22:42:49 -0400

    #5 Dynamic Types

--- sandbox.ts
+++ sandbox.ts
@@ -1,13 +1,35 @@
-let character = "mario";
-let age = 30;
-let isBlackBelt = false;
+//arrays
+let names = ["luigi", "mario", "yoshi"];
 
-character = "luigi";
-age = 40;
-isBlackBelt = true;
+// names = "van";//wont work
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
+names.push("toad");
+//names.push(3); //wont work
+names[0] = "van";
+
+let numbers = [10, 20, 30];
+numbers.push(25);
+// numbers.push("shaun");//wont work
+numbers[1] = 3;
+
+let mixed = ["ken", 4, "chun-li", 8, 9];
+mixed.push("ryu");
+mixed.push(10);
+mixed[0] = 3;
+
+//objects
+let ninja = {
+  name: "mario",
+  belt: "belt",
+  age: 30,
 };
+ninja.age = 40;
+ninja.name = "ryu";
+// ninja.age = "30";//wont work
+// ninja.skills = ["fighting", "sneaking"]; //wont work
 
-console.log(circ(7.5));
+ninja = {
+  name: "yoshi",
+  belt: "orange",
+  age: 40,
+};
