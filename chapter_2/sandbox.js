cb8e17acd9f5aa50eebd43a64fbea2b60afdaf7a
vanguyen333 <vanguyen333@yahoo.com>
2023-09-12 21:43:19 -0400

    #31:18

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,13 @@
+// Strings
+
+let firstName = "BandonSon";
+// console.log(firstName[0]);
+//string length
+// console.log(firstName.length);
+//string method
+// console.log(firstName.toUpperCase(0));
+
+let index = firstName.indexOf("S");
+console.log(index);
+
+//common string methods
