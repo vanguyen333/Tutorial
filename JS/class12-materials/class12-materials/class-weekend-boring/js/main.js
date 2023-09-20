cec7c989823058bcf9f662182147b2c8e7f0458c
vanguyen333 <vanguyen333@yahoo.com>
2023-09-20 16:32:09 -0400

    modified class weekend boring, done homework with toLowerCase()

--- JS/class12-materials/class12-materials/class-weekend-boring/js/main.js
+++ JS/class12-materials/class12-materials/class-weekend-boring/js/main.js
@@ -1,10 +1,16 @@
-document.querySelector('#check').addEventListener('click', check)
+document.querySelector("#check").addEventListener("click", check);
 
 function check() {
-
-  const day = document.querySelector('#day').value
+  const day = document.querySelector("#day").value.toLowerCase();
 
   //Conditionals go here
-
-
+  if (day === "monday") {
+    document.querySelector("#placeToSee").innerText = "energetic";
+  } else if (day === "tuesday" || day === "wednesday") {
+    document.getElementById("placeToSee").innerText = "magical";
+  } else if (day === "thursday" || day === "friday") {
+    document.getElementById("placeToSee").innerText = "Woo Hoo";
+  } else {
+    document.getElementById("placeToSee").innerText = "enjoy!";
+  }
 }
--- JS/class12-materials/class12-materials/simple-calculator/js/main.js
+++ JS/class12-materials/class12-materials/simple-calculator/js/main.js
@@ -1,26 +1,27 @@
-let total = 0
-
-document.querySelector('#pumpkin').addEventListener('click', makeZero)
-document.querySelector('#dominosPizza').addEventListener('click', jumanji)
-document.querySelector('#zebra').addEventListener('click', add9)
-document.querySelector('#cantThinkOfAnything').addEventListener('click', sub2)
+let total = 0;
+//https://www.youtube.com/watch?v=_A20kVsaqIk&list=PLBf-QcbaigsJysJ-KFZvLGJvvW-3sfk1S&index=989
+// 2:00:04
+document.querySelector("#pumpkin").addEventListener("click", makeZero);
+document.querySelector("#dominosPizza").addEventListener("click", jumanji);
+document.querySelector("#zebra").addEventListener("click", add9);
+document.querySelector("#cantThinkOfAnything").addEventListener("click", sub2);
 
 function makeZero() {
-  total = 0
-  document.querySelector('#placeToPutResult').innerText = total
+  total = 0;
+  document.querySelector("#placeToPutResult").innerText = total;
 }
 
 function jumanji() {
-  total = total + 3
-  document.querySelector('#placeToPutResult').innerText = total
+  total = total + 3;
+  document.querySelector("#placeToPutResult").innerText = total;
 }
 
 function add9() {
-  total = total + 9
-  document.querySelector('#placeToPutResult').innerHTML = total
+  total = total + 9;
+  document.querySelector("#placeToPutResult").innerHTML = total;
 }
 
 function sub2() {
-  total = total - 2
-  document.querySelector('#placeToPutResult').innerHTML = total
+  total = total - 2;
+  document.querySelector("#placeToPutResult").innerHTML = total;
 }
