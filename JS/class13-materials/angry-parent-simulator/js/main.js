45d779777a770e55cae66ed32239a98005d3dec7
vanguyen333 <vanguyen333@yahoo.com>
2023-09-21 19:37:22 -0400

    celsius to fahrenheit exercise done

--- JS/class13-materials/angry-parent-simulator/js/main.js
+++ JS/class13-materials/angry-parent-simulator/js/main.js
@@ -1,16 +1,16 @@
 // My Own version
+//class#13 2:00 https://www.youtube.com/watch?v=pS6ykGL-fRE&t=5400s
+// document.querySelector("#yell").addEventListener("click", yell);
 
-document.querySelector("#yell").addEventListener("click", yell);
-
-function yell() {
-  const fName = document.querySelector("#firstName").value;
-  const fMiddle = document.querySelector("#firstMiddle").value;
-  const lMiddle = document.querySelector("#lastMiddle").value;
-  const lName = document.querySelector("#lastName").value;
-  document.querySelector(
-    "#placeToYell"
-  ).innerText = `${fName} ${fMiddle} ${lMiddle} ${lName}`;
-}
+// function yell() {
+//   const fName = document.querySelector("#firstName").value;
+//   const fMiddle = document.querySelector("#firstMiddle").value;
+//   const lMiddle = document.querySelector("#lastMiddle").value;
+//   const lName = document.querySelector("#lastName").value;
+//   document.querySelector(
+//     "#placeToYell"
+//   ).innerText = `${fName} ${fMiddle} ${lMiddle} ${lName}`;
+// }
 
 // document.querySelector("#yell").addEventListener("click", run);
 
@@ -29,20 +29,20 @@ function yell() {
 //   ).innerText = `${fName} ${fMidName} ${lMidName} ${lName}`;
 // }
 
-// const synth = window.speechSynthesis;
-// document.querySelector('#yell').addEventListener('click', run)
-//
-// function run() {
-//   const fName = document.querySelector('#firstName').value
-//   const fMidName = document.querySelector('#firstMiddle').value
-//   const lMidName = document.querySelector('#lastMiddle').value
-//   const lName = document.querySelector('#lastName').value
-//
-//   const yellText =  `${fName} ${fMidName} ${lMidName} ${lName}`
-//
-//   document.querySelector('#placeToYell').innerText = yellText
-//
-//   let yellThis = new SpeechSynthesisUtterance(yellText);
-//
-//   synth.speak(yellThis);
-// }
+const synth = window.speechSynthesis;
+document.querySelector("#yell").addEventListener("click", run);
+
+function run() {
+  const fName = document.querySelector("#firstName").value;
+  const fMidName = document.querySelector("#firstMiddle").value;
+  const lMidName = document.querySelector("#lastMiddle").value;
+  const lName = document.querySelector("#lastName").value;
+
+  const yellText = `${fName} ${fMidName} ${lMidName} ${lName}`;
+
+  document.querySelector("#placeToYell").innerText = yellText;
+
+  let yellThis = new SpeechSynthesisUtterance(yellText);
+
+  synth.speak(yellThis);
+}
--- JS/class13-materials/temperature-converter/js/main.js
+++ JS/class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,15 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+// Celsius to F
+// enter input,grab input(value), calculate input to F,
+// Display result to screen
+
+// from teacher psueduo code
+// need the value that is in celsius
+document.querySelector("#yell").addEventListener("click", convert);
+function convert() {
+  let temp = document.querySelector("#celsius").value;
+  // convert from celsius to fahrenheit
+  temp = (temp * 9) / 5 + 32;
+  // show it
+  document.querySelector("#placeToYell").innerText = temp;
+}
