afeeafb79b8911e81e778567d74c8b50c433f50e
vanguyen333 <vanguyen333@yahoo.com>
2023-09-22 15:48:50 -0400

    #14 temp converter exercise

--- JS/class14-materials/temperature-converter/js/main.js
+++ JS/class14-materials/temperature-converter/js/main.js
@@ -1,1 +1,13 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+//only onClick
+document.querySelector("#yell").addEventListener("click", convert);
+
+function convert() {
+  //   console.log("hello");//just to check if the function works
+  //grab value of input
+  let tempt = document.querySelector("#temp").value;
+  //convert C to F
+  tempt = (tempt * 9) / 5 + 32;
+  //show result
+  document.querySelector("#placeToYell").innerText = tempt;
+}
