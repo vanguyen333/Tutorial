1e82607a380af4fee3ee730bdeb4a1cb5d911d94
vanguyen333 <vanguyen333@yahoo.com>
2023-09-22 16:01:18 -0400

    #14 the barchelor 2 exercise

--- JS/class14-materials/bachelor-code/1/js/main.js
+++ JS/class14-materials/bachelor-code/1/js/main.js
@@ -1,6 +1,16 @@
-document.querySelector('#finalRose').addEventListener('click', /*INSERTCODE*/)
+// document.querySelector("#finalRose").addEventListener("click", hide);
 
-function hide(){
-	document.querySelector(/*INSERTCODE*/).style.display = 'none'
-	document.querySelector(/*INSERTCODE*/).style.display = 'none'
+// function hide() {
+//   document.querySelector("#claire").style.display = "none";
+//   document.querySelector("#sharleen").style.display = "none";
+// }
+
+//click the title
+document.querySelector("#finalRose").addEventListener("click", hide);
+
+//run the function to hide claire and sharleen
+function hide() {
+  document.getElementById("claire").style.display = "none";
+  document.getElementById("sharleen").style.display = "none";
 }
+getElementById;
