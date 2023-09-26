bb99bada2de48aae5f41484c52739e917093972a
vanguyen333 <vanguyen333@yahoo.com>
2023-09-26 18:02:10 -0400

    #16 review-in-summer-game

--- JS/class16-materials2/review-summer-games/js/main.js
+++ JS/class16-materials2/review-summer-games/js/main.js
@@ -1,1 +1,14 @@
-//Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+//Create a function that takes in an array of numbers.
+//  Multiply each number together and alert the product.
+function arrayCheck(arr) {
+  let product = 1;
+  //   for (let i = 1; i < arr.length; i++) {
+  //     product *= arr[i];
+  //    }
+  //     console.log(product);
+  arr.forEach((item, index) => {
+    product *= item;
+  });
+  console.log(product);
+}
+arrayCheck([1, 2, 3]);
