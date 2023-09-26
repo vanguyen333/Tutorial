4329586ce1c40cd60735f5f9fe557f19913b0229
vanguyen333 <vanguyen333@yahoo.com>
2023-09-26 18:41:06 -0400

    #16 review-winter-game

--- JS/class16-materials2/review-winter-games/js/main.js
+++ JS/class16-materials2/review-winter-games/js/main.js
@@ -1,1 +1,22 @@
-//Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+//Create a function that takes in an array of numbers.
+// Return a new array containing every even number
+// from the original array (do not use map or filter)
+function evenNumCheck(num) {
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] % 2 === 0) {
+      console.log(num[i]);
+    }
+  }
+}
+evenNumCheck([1, 2, 3, 4, 5]);
+//solution from teacher
+function newArrEven(arr) {
+  let newArr = [];
+  arr.forEach((item, index) => {
+    if (item % 2 === 0) {
+      newArr.push(item);
+    }
+  });
+  return newArr;
+}
+console.log(newArrEven([4, 5, 6, 7, 8, 9]));
