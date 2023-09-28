31ef1873be8394b0706996c3b040385af91803f4
vanguyen333 <vanguyen333@yahoo.com>
2023-09-28 15:59:08 -0400

    #18 objects-lost-galazy

--- JS/class18-materials/objects-lost-galaxy/js/main.js
+++ JS/class18-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,31 @@
 //Create a mouse object that has four properties and three methods
+let mouse = {
+  color: "white",
+  size: "small",
+  breed: "lab",
+  tail: "none",
+  eat: function (food) {
+    return food;
+  },
+  drink: function (type) {
+    return type;
+  },
+  run: function () {
+    return "run very fast";
+  },
+};
+
+let chuot = {};
+chuot.brand = "logitech";
+chuot.ergo = true;
+chuot.model = "mx ergo";
+chuot.trackBall = true;
+chuot.leftClick = function () {
+  console.log("Click left");
+};
+chuot.rightClick = function () {
+  console.log("Click right");
+};
+chuot.scrollWheel = function () {
+  console.log("Jump");
+};
--- JS/class18-materials/space-review/js/main.js
+++ JS/class18-materials/space-review/js/main.js
@@ -1,12 +1,36 @@
 //Arrays
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
+let arr = [1, 2, 3, 4, 5, 6];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   sum = sum + arr[i];
+// }
 
+// console.log(sum);
+//another way
+let sum = arr.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  0
+);
+console.log(sum);
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
-
+function check(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i] * arr[i]);
+  }
+}
+check([1, 2, 3]);
 //Create a function that takes string
 //Print the reverse of that string to the console
-
+function reverseWord(word) {
+  let reversedword = word.reverse().join("");
+  console.log(reversedword);
+}
+reverseWord("Hello");
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+function str(word) {
+  alert;
+}
