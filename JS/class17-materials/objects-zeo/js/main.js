6632a95c4133aae419099db1a670826c3291989d
vanguyen333 <vanguyen333@yahoo.com>
2023-09-28 12:44:18 -0400

    #17 objects-zeo

--- JS/class17-materials/objects-zeo/js/main.js
+++ JS/class17-materials/objects-zeo/js/main.js
@@ -1,1 +1,17 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {
+  color: "black",
+  brand: "nike",
+  shape: "round",
+  size: "large",
+  function: (start = () => {
+    console.log("STARTING");
+  }),
+  function: (stop = () => {
+    console.log("STOPPING");
+  }),
+  function: () => {
+    alert(this.brand);
+  },
+};
+stopwatch.stop;
