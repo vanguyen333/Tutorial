0b6a40ff4f2d5a1c99b45084d6e2220c7d6f1228
vanguyen333 <vanguyen333@yahoo.com>
2023-10-02 22:37:22 -0400

    #20 objects-dog-photos

--- JS/class20-materials_/objects-dog-photos/js/main.js
+++ JS/class20-materials_/objects-dog-photos/js/main.js
@@ -1,1 +1,12 @@
-//Get a dog photo from the dog.ceo api and place the photo in the DOM
+//Get a dog photo from the dog.ceo api and
+
+const url = "https://dog.ceo/api/breeds/image/random";
+fetch(url)
+  .then((res) => res.json()) //parse response as JSON
+  .then((data) => {
+    console.log(data.message);
+    document.querySelector("img").src = data.message;
+  })
+  .catch((err) => {
+    console.log(`error ${err}`);
+  });
