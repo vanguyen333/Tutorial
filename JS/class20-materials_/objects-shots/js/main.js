04a1617889a8367cf8e010924826c5f6b05b687f
vanguyen333 <vanguyen333@yahoo.com>
2023-10-03 18:58:54 -0400

    #20 Object shots API drinks

--- JS/class20-materials_/objects-shots/js/main.js
+++ JS/class20-materials_/objects-shots/js/main.js
@@ -1,1 +1,23 @@
-//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+//The user will enter a cocktail.
+//  Get a cocktail name, photo,
+//  and instructions and place them in the DOM
+// const url = "www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
+
+document.querySelector("button").addEventListener("click", getDrink);
+
+function getDrink() {
+  let drinkChoice = document.querySelector("input").value;
+  const url =
+    "https://thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkChoice;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.drinks[0]);
+      document.querySelector("h2").innerText = data.drinks[0].strDrink;
+      document.querySelector("img").src = data.drinks[0].strDrinkThumb;
+      document.querySelector("h3").innerText = data.drinks[0].strInstructions;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
